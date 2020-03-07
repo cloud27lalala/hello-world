be7d2b19ce4ea995c6255aa3f458295c6c00e175
cloud27lalala <cloud27dragon@foxmail.com>
2020-03-07 20:26:44 +0800

    helloworld add commit

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,15 @@
+/*
 #include "HelloWorld.h"
 #include <iostream>
 
 void HelloWorld::hello() {
   std::cout << "Hello World!" << std::endl;
 }
-
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
