00e3cf43d1e510a10741d7028886d3671afbb37f
cloud27lalala <cloud27dragon@foxmail.com>
2020-03-12 18:58:45 +0800

    SuperPrime add commit

--- SuperPrime.c
+++ SuperPrime.c
@@ -1,10 +1,57 @@
-//超级素数：它本身，各位数字的和，各位数字的平方和，都是素数。
-//求 100~10000 内的所有超级素数，及它们的平均数。
+#include<stdio.h>
+#include<math.h>
 
-int isPrime(int x);
-int splitNum(int x, int num[]);
+int isprime(int x)
+{
+	for (int i=2;i<=sqrt(x);i++)
+	{
+		if (x%i==0)
+		  return 0;
+	}
+	return 1;
+}
+void splitnum(int x,int a[])
+{
+	int n=1,m=10;
+	int y;
+	y=x;
+	while(y%10>0)
+	{
+		n++;
+		y=y/10;
+	}
+	a[0]=n;
+	for (int i=1;i<=n;i++)
+	{
+		a[n-i+1]=x%m;
+		m*=10;
+	}
+}
 
 int main()
 {
-    return 0;
+	float sum=0,n=0;
+	int arg;
+	for (int i=100;i<=10000;i++)
+	{
+		if (isprime(i)==1)
+		{
+			int a[6]={0};//a[0]Ϊλ�� 
+			int sum1=0,sum2=0;
+			splitnum(i,a);
+			for (int j=1;j<=a[0];j++)
+			{
+				sum1+=a[j];
+				sum2+=a[j]*a[j];
+			}
+			if ( isprime(sum1) && isprime(sum2) )
+			{
+				printf("%d\n",i);
+				sum+=i;
+				n++;
+			}
+		}
+	}
+	printf("%.2f\n",sum/n);
+	return 0;
 }
