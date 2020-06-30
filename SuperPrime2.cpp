a1b322c29d9db5057f06f3c15c3bea1205c6448d
cloud27lalala <cloud27dragon@foxmail.com>
2020-06-30 15:38:12 +0800

    0403

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,3 +1,4 @@
+/*
 //作业：删除多余的集合类，使用继承方法实现相同的功能。 
 #include <iostream>
 class Prime {
@@ -92,4 +93,62 @@ int main() {
   else
     std::cout << "113 is NOT SuperPrime" << std::endl;
   return 0;
+}
+*/
+#include<stdio.h>
+#include<math.h>
+
+int isprime(int x)
+{
+	for (int i=2;i<=sqrt(x);i++)
+	{
+		if (x%i==0)
+		  return 0;
+	}
+	return 1;
+}
+void splitnum(int x,int a[])
+{
+	int n=1,m=10;
+	int y;
+	y=x;
+	while(y%10>0)
+	{
+		n++;
+		y=y/10;
+	}
+	a[0]=n;
+	for (int i=1;i<=n;i++)
+	{
+		a[n-i+1]=x%m;
+		m*=10;
+	}
+}
+
+int main()
+{
+	float sum=0,n=0;
+	int arg;
+	for (int i=100;i<=10000;i++)
+	{
+		if (isprime(i)==1)
+		{
+			int a[6]={0};//a[0]为位数 
+			int sum1=0,sum2=0;
+			splitnum(i,a);
+			for (int j=1;j<=a[0];j++)
+			{
+				sum1+=a[j];
+				sum2+=a[j]*a[j];
+			}
+			if ( isprime(sum1) && isprime(sum2) )
+			{
+				printf("%d\n",i);
+				sum+=i;
+				n++;
+			}
+		}
+	}
+	printf("%.2f\n",sum/n);
+	return 0;
 }
